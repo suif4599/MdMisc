50146de9ffe1cdc470fcff9f1b60f4e2ac6db9c5
suif4599 <suif4599@gmail.com>
2026-07-07 22:49:03 +0800

    chore: add mdcss as an independent repo

--- mdcss/templates/parser/preparser_fence_restore.js
+++
@@ -1,8 +0,0 @@
-const __mdcssFenceTokenRestorePattern = /@@MDCSS_FENCE_BLOCK_(\d+)@@/g;
-markdown = markdown.replace(__mdcssFenceTokenRestorePattern, (match, indexText) => {
-    const index = Number(indexText);
-    if (!Number.isInteger(index) || index < 0 || index >= __MDCSS_FENCED_BLOCKS__.length) {
-        return match;
-    }
-    return __MDCSS_FENCED_BLOCKS__[index];
-});
